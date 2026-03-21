a36cf254af2fff0af48685e6859c26eb10668b76
Radin Pourfarzi <97715025+RadinPourfarzi@users.noreply.github.com>
2026-03-20 19:13:33 -0700

    Added Introduction to lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Radin Pourfarzi" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hello everyone! I am Radin Pourfarzi, and I am a Junior from California.\nI am interested in learning computer science, and more specifically, I would like to learn AI.\nI am intrigued by the potential of the new technologies.\nI enjoy reading in my free time: fantasy, academic books, or Orwell (he's my favorite author).")
 
 if __name__ == "__main__":
     main()
